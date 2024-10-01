7e5b4f3c057c92ecfd0cd890b29202ebad18c5fe
vmandany <136026388+vmandany@users.noreply.github.com>
2024-09-30 20:13:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.querySelector('.nav-links');
-
-    // Toggle the display of the mobile menu
     hamburger.addEventListener('click', () => {
         navLinks.classList.toggle('active');
     });
